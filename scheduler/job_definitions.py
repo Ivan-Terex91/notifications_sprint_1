8d1f1ccb45fcc759537ec39410aba84a58d22a60
Orkhan <oguseynov@users.noreply.github.com>
2021-07-29 19:31:44 +0200

    Implemented scheduler, only job implementations and rabbit producers left

---
+++ scheduler/job_definitions.py
@@ -0,0 +1,6 @@
+def new_films_of_week(var_one, var_two):
+    print("new_films_of_week")
+
+
+def saved_films(var_one, var_two):
+    print("saved_films")
---
+++ scheduler/jobs.py
@@ -0,0 +1,20 @@
+jobs_list = [
+    {
+        "id": "job1",
+        "func": "job_definitions:new_films_of_week",
+        "replace_existing": True,
+        "args": (1, 2),
+        "trigger": "cron",
+        "day_of_week": "fri",
+        "hour": 18,
+    },
+    {
+        "id": "job2",
+        "func": "job_definitions:saved_films",
+        "replace_existing": True,
+        "args": (1, 2),
+        "trigger": "cron",
+        "day": "1st sat",
+        "hour": 18,
+    },
+]
