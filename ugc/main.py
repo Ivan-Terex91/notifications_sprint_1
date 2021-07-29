5c67bb1817f331985e09bd1eba1d8b64edd3b556
Orkhan <oguseynov@users.noreply.github.com>
2021-07-29 21:27:15 +0200

    Fixed import sorting issue

--- ugc/main.py
+++ ugc/main.py
@@ -1,10 +1,10 @@
 import sentry_sdk
 import uvicorn
+from api.v1 import bookmark, rating, review
+from core import auth, config, mongo
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from motor.motor_asyncio import AsyncIOMotorClient
-from api.v1 import bookmark, rating, review
-from core import auth, config, mongo
 
 sentry_sdk.init(
     dsn=config.SENTRY_DSN,
