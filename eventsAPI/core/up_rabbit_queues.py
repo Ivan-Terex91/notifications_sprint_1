b051a0e063f58c2acca7f821bd968722bbb45500
Orkhan <oguseynov@users.noreply.github.com>
2021-07-31 04:36:11 +0200

    Another mypy errors

--- eventsAPI/core/up_rabbit_queues.py
+++ eventsAPI/core/up_rabbit_queues.py
@@ -3,12 +3,13 @@ import os
 from typing import List
 
 import aio_pika
+
 import config
 
 
 async def declare_queues(queues: List[str]):
     """Объявляем очереди до старта api"""
-    connection = await aio_pika.connect_robust(url=config.RABBIT_DSN)
+    connection: aio_pika.RobustConnection = await aio_pika.connect_robust(url=config.RABBIT_DSN)
     async with connection.channel() as channel:
         for queue in queues:
             await channel.declare_queue(name=queue, durable=True)
